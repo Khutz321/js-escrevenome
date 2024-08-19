82903a809180d994994541f9160d9d798fdb6340
Khutz321 <115053107+Khutz321@users.noreply.github.com>
2024-08-18 22:05:18 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+    createCanvas(600, 600);
+    background("white");
+  }
+  
+  function draw() {  
+    stroke ("pink")
+    fill ("red")
+    
+   // console.log(mouseIsPressed);
+    
+    if(mouseIsPressed)
+    rect(mouseX, mouseY, 28, 35)
+   }
